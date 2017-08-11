dc8d921611972e10708d25cddf37b50e9f5f5564
Oliver Favell <OliverFavell@etech.net>
2017-08-11 16:44:15 +0100

    Minor refactor

--- src/convertor.js
+++ src/convertor.js
@@ -1,15 +1,14 @@
 const csv = require('csvtojson')
-const appender = require('./append')
+const append = require('./appender')
 
 module.exports = (index, csvPath, savePath) => {
-  let appenderService = appender(index)
+  let appenderService = append(index)
   let indexedJson = {}
   console.time("[Rubberband] Converting")
   return new Promise((resolve, reject) => {
     csv()
       .fromFile(csvPath)
       .on('end_parsed', (jsonObjArr) => {
-        console.log("[Rubberband] CSV converted")
         console.timeEnd("[Rubberband] Converting")
         let indexedJson = appenderService.generateIndex(jsonObjArr, savePath)
         resolve(indexedJson)
--- src/queryer.js
+++ src/queryer.js
@@ -1,34 +1,35 @@
 const request = require('request')
 const compare = require('string-similarity')
+
 const address = "http://192.168.70.56:9200"
 const reqMethod = "POST"
 const elasticParams = "/epc/_search"
 
+const postcodeWeighting = {
+  0.99: 0,
+  0.95: 2,
+  0.81: 7,
+  0.61: 12,
+  0: 31
+}
+const countyWeighting = {
+  0.99: 0,
+  0.80: 1,
+  0.60: 3,
+  0.40: 5,
+  0: 8
+}
+const addressWeighting = {
+  0.99: 0,
+  0.95: 1,
+  0.82: 5,
+  0.59: 14,
+  0: 31
+}
+
 module.exports = (queryString) => {
 
-  let postcodeWeighting = {
-    0.99: 0,
-    0.95: 2,
-    0.81: 7,
-    0.61: 12,
-    0: 31
-  }
-  let countyWeighting = {
-    0.99: 0,
-    0.80: 1,
-    0.60: 3,
-    0.40: 5,
-    0: 8
-  }
-  let addressWeighting = {
-    0.99: 0,
-    0.95: 1,
-    0.82: 5,
-    0.59: 14,
-    0: 31
-  }
-
-  let queryBody = '{  "query": {    "multi_match": {      "query": "' + queryString + '",      "minimum_should_match": 5,      "type": "cross_fields",      "fields": ["ADDRESS1^3", "ADDRESS2", "ADDRESS3", "POSTCODE^2", "COUNTY^2"]    }  }}'
+  let queryBody = '{ "query": { "multi_match": { "query": "' + queryString + '", "minimum_should_match": 5, "type": "cross_fields", "fields": ["ADDRESS1^3", "ADDRESS2", "ADDRESS3", "POSTCODE^2", "COUNTY^2"] }}}'
   let options = {
     url: address + elasticParams,
     method: reqMethod,
@@ -48,30 +49,9 @@ module.exports = (queryString) => {
         })
         console.log(hit._source.ADDRESS + " - " + hit._source.POSTCODE)
         let accuracyCount = 100
-
-        // FOR TESTING
-        // let postalAcc = compare.compareTwoStrings(hit._source.POSTCODE, searchTerms.pop())
-        // let postalDeduction = calculateAccuracyDeduction(postalAcc, postcodeWeighting)
-        // let countyAcc = compare.compareTwoStrings(hit._source.COUNTY, searchTerms.pop())
-        // let countyDeduction = calculateAccuracyDeduction(countyAcc, countyWeighting)
-        // let addressAcc = compare.compareTwoStrings(hit._source.ADDRESS, searchTerms.join())
-        // let addressDeduction = calculateAccuracyDeduction(addressAcc, addressWeighting)
-        //
-        // console.log("- Postcode Accuracy", postalAcc)
-        // console.log("- Postcode deduction", postalDeduction)
-        // console.log("- County Accuracy", countyAcc)
-        // console.log("- County deduction", countyDeduction)
-        // console.log("- Address Accuracy", addressAcc)
-        // console.log("- Address deduction", addressDeduction)
-        // END TESTING
-
-        // FOR USE AFTER TESTING
         accuracyCount -= calculateAccuracyDeduction(compare.compareTwoStrings(hit._source.POSTCODE, searchTerms.pop()), postcodeWeighting)
         accuracyCount -= calculateAccuracyDeduction(compare.compareTwoStrings(hit._source.COUNTY, searchTerms.pop()), countyWeighting)
         accuracyCount -= calculateAccuracyDeduction(compare.compareTwoStrings(hit._source.ADDRESS, searchTerms.join()), addressWeighting)
-        // Add in ADDRESS1 checking, and if it correctly matches no reduction, if it doesn't match slight reduction for balancing houses on same street
-        // accuracyCount -= (postalDeduction + countyDeduction + addressDeduction)
-
         hit._accuracy = accuracyCount
         console.log("- Accuracy",hit._accuracy + "%\n")
         return hit
